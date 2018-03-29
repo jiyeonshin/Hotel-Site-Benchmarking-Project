fb59f545756c8327a2ddf949f856a9a755862956
soy <thdusdl8044@gmail.com>
2018-03-29 17:46:30 +0900

    step2opt js 수정
    step2opt js 수정

--- 전체/js/step2opt.js
+++ 전체/js/step2opt.js
@@ -1,89 +1,116 @@
 $(function() {
-            $("#rsvoption").hide();
+        $("#rsvoption").hide();
             
-            $('#optpri_buf').append("원");
-            $('#optpri_bed').append("원");
-            $('#total').append("원");
+        $('#optpri_buf').append("원");
+        $('#optpri_bed').append("원");
+        $('#total').append("원");
                         
-            // 옵션 가격과 예약 가격 일치or불일치
-            var buffet1 = $('#optpri_buf').text();
-            var buffet2 = $('#rsvoptpri_buf').text();
-            if(buffet1 !== buffet2) {
-                $('#rsvoptpri_buf').text(buffet1);
-            }else {
-                alert("가격 불일치");
-            }
+        // 옵션 가격과 예약 가격 일치or불일치
+        var buffet1 = $('#optpri_buf').text();
+        var buffet2 = $('#rsvoptpri_buf').text();
+        if(buffet1 !== buffet2) {
+            $('#rsvoptpri_buf').text(buffet1);
+        }else {
+             alert("가격 불일치");
+        }
             
-            var buffet3 = $('#optpri_bed').text();
-            var buffet4 = $('#rsvoptpri_bed').text();
-            if(buffet3 !== buffet4) {
-                $('#rsvoptpri_bed').text(buffet3);
-            }else {
-                alert("가격 불일치");
-            }
+        var bed1 = $('#optpri_bed').text();
+        var bed2 = $('#rsvoptpri_bed').text();
+        if(bed1 !== bed2) {
+            $('#rsvoptpri_bed').text(bed1);
+        }else {
+            alert("가격 불일치");
+        }
 })
         
 function onClick1(check){
-           var ischecked = check.checked;
+    var ischecked = check.checked;
             
-            var bufVal = parseInt($('#optpri_buf').text());
-            var total = parseInt($('#total').text());
-
-            var sum
+    var buffet = $('#optpri_buf').text();
+    var total = $('#total').text();
+    
+    var buffetSplit = buffet.split(","); 
+    var totalSplit = total.split(",");
+    var buffetStr = parseInt(buffetSplit[0].concat(buffetSplit[1]));
+    var totalStr = parseInt(totalSplit[0].concat(totalSplit[1])); 
+    
+    var sum
             
-           if(check.checked){
-               $("#rsvoption").show();
-               $(".buffet").show();
+    if(check.checked){
+        $("#rsvoption").show();
+        $(".buffet").show();
+        
+        sum = String(totalStr+buffetStr);
+        var sum1 = sum.substr(0,3);
+        var sum2 = sum.substr(sum.length-3,3);
+        var endSum = sum1.concat(","+sum2);
+        $('#total').text(endSum);
+        $('#total').append(" 원");
                
-               sum = total+bufVal;
-               $('#total').text(sum);
-               $('#total').append(" 원");
-               
-               if($('#package2').is(":checked")){
-                   $(".bed").show();
-               }else {
-                    $(".bed").hide();
-               }
-           }else{
-             if($('#package2').is(":checked")){
-                 $(".buffet").hide();
-             }else {
-                 $("#rsvoption").hide();
-             }
-               sum = total-bufVal;
-               $('#total').text(sum);
-               $('#total').append(" 원");
-           }
+        if($('#package2').is(":checked")){
+           $(".bed").show();
+        }else {
+            $(".bed").hide();
+        }
+    }else{
+        if($('#package2').is(":checked")){
+            $(".buffet").hide();
+        }else {
+            $("#rsvoption").hide();
+        }
+        sum = String(totalStr-buffetStr);
+        console.log(sum, typeof sum);
+        var sum1 = sum.substr(0,3);
+        var sum2 = sum.substr(sum.length-3,3);
+        var endSum = sum1.concat(","+sum2);
+        
+        $('#total').text(endSum);
+        $('#total').append(" 원");
+    }
 }
         
 function onClick2(check){
-           var ischecked = check.checked;
+    var ischecked = check.checked;
             
-            var bedVal = parseInt($('#optpri_bed').text());
-            var total = parseInt($('#total').text());
-            var sum
+    var bed = $('#optpri_bed').text();
+    var total = $('#total').text();
+    
+    var bedSplit = bed.split(",");
+    var totalSplit = total.split(",");
+    var bedStr = parseInt(bedSplit[0].concat(bedSplit[1]));
+    var totalStr = parseInt(totalSplit[0].concat(totalSplit[1]));
+    
+    var sum
             
-           if(check.checked){
-               $("#rsvoption").show();
-               $(".bed").show();
+    if(check.checked){
+        $("#rsvoption").show();
+        $(".bed").show();
                
-               sum = total+bedVal;
-               $('#total').text(sum);
-               $('#total').append(" 원");
-               
-               if($('#package1').is(":checked")){
-                   $(".buffet").show();
-               }else {
-                    $(".buffet").hide();
-               }
-           }else{
-               if($('#package1').is(":checked")){
-                    $(".bed").hide();
-               }else {
-                   $("#rsvoption").hide();
-               }
-               sum = total-bedVal;
-               $('#total').text(sum);
-               $('#total').append(" 원");
-           }
+        sum = String(totalStr+bedStr);
+        var sum1 = sum.substr(0,3);
+        var sum2 = sum.substr(sum.length-3,3);
+        var endSum = sum1.concat(","+sum2);
+        
+        $('#total').text(endSum);
+        $('#total').append(" 원");
+           
+       if($('#package1').is(":checked")){
+           $(".buffet").show();
+       }else {
+           $(".buffet").hide();
+       }
+    }else{
+        if($('#package1').is(":checked")){
+            $(".bed").hide();
+        }else {
+            $("#rsvoption").hide();
+        }
+        sum = String(totalStr-bedStr);
+        var sum1 = sum.substr(0,3);
+        var sum2 = sum.substr(sum.length-3,3);
+        var endSum = sum1.concat(","+sum2);
+        
+        $('#total').text(endSum);
+        $('#total').append(" 원");
+    }
 }
